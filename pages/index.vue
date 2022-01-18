41261171a286dc0f8ccbf8d2d48eaa2f2f7115b9
francisco <fr.betancourt@live.com>
2022-01-18 13:30:35 -0300

    Added hero title and subtitle.

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
+  <div class="container is-fullhd">
+    <div class="is-flex is-flex-direction-column is-align-items-center">
+      <h1 class="title is-1">
+        Title 1
+      </h1>
+      <h3 class="subtitle is-3">
+        Subtitle 3
+      </h3>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,3 +18,6 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+<style lang="scss" scoped>
+
+</style>
