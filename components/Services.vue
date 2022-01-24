7367d52e2532eed30a8a20002080c1a984297e59
francisco <fr.betancourt@live.com>
2022-01-24 12:58:01 -0300

    Added reversed direction to web app.

--- components/Services.vue
+++ components/Services.vue
@@ -1,17 +1,29 @@
 <template>
     <section class="container is-full-hd services">
-      <div class="columns">
-          <div class="column">
+        <div class="columns">
+            <div class="column">
+                <h2 class="title is-2">
+                    Apps mobile
+                </h2>
+                <h5 class="subtitle is-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem suscipit, quo laborum fugiat repudiandae veritatis non! Eos aliquid quo beatae dicta facere voluptatibus suscipit quisquam natus mollitia possimus, culpa ratione?
+                </h5>
+            </div>
+            <div class="column">
+            </div>
+        </div>
+        <div class="columns web-app">
+            <div class="column">
             <h2 class="title is-2">
-                Apps mobile
+                Apps web
             </h2>
             <h5 class="subtitle is-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem suscipit, quo laborum fugiat repudiandae veritatis non! Eos aliquid quo beatae dicta facere voluptatibus suscipit quisquam natus mollitia possimus, culpa ratione?
             </h5>
-          </div>
-          <div class="column">
-          </div>
-      </div>
+            </div>
+            <div class="column">
+            </div>
+       </div>
     </section>
 </template>
 
@@ -22,9 +34,18 @@
 </script>
 
 <style lang="scss" scoped>
+    
+    @import '~bulma/sass/utilities/mixins';
+
     .services {
         margin-top: 4rem;
         padding-left: 1rem;
         padding-right: 1rem;
+    }
+
+    @include desktop {
+        .web-app {
+            flex-direction: row-reverse;
+        }
     }
 </style>
