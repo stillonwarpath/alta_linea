17b7b3caec55adbfa69dc88eaf616a99eeb357aa
francisco <fr.betancourt@live.com>
2022-01-24 12:04:27 -0300

    Added Hero component.

---
+++ components/Hero.vue
@@ -0,0 +1,47 @@
+<template>
+     <div class="container is-fullhd">
+    <!--Hero-->
+    <section>
+      <div class="is-flex is-flex-direction-column is-align-items-center first-title">
+        <h1 class="title is-1">
+          Title 1
+        </h1>
+        <h3 class="subtitle is-3">
+          Subtitle 3
+        </h3>
+      </div>
+      <div class="is-flex is-flex-direction-column is-align-items-center second-title">
+        <h2 class="title is-2">
+          Title 2
+        </h2>
+        <h4 class="subtitle is-4">
+          Subtitle 4
+        </h4>
+      </div>
+    </section>
+    <!--Services-->
+    <section>
+      <h2 class="title is-2">
+        Apps mobile
+      </h2>
+      <h5 class="subtitle is-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem suscipit, quo laborum fugiat repudiandae veritatis non! Eos aliquid quo beatae dicta facere voluptatibus suscipit quisquam natus mollitia possimus, culpa ratione?
+      </h5>
+    </section>
+  </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+  .first-title {
+    margin-top: 4rem;
+  }
+  .second-title {
+    margin-top: 6rem;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,5 @@
 <template>
-  <div class="container is-fullhd">
-    <!--Hero-->
-    <section>
-      <div class="is-flex is-flex-direction-column is-align-items-center first-title">
-        <h1 class="title is-1">
-          Title 1
-        </h1>
-        <h3 class="subtitle is-3">
-          Subtitle 3
-        </h3>
-      </div>
-      <div class="is-flex is-flex-direction-column is-align-items-center second-title">
-        <h2 class="title is-2">
-          Title 2
-        </h2>
-        <h4 class="subtitle is-4">
-          Subtitle 4
-        </h4>
-      </div>
-    </section>
-    <!--Services-->
-    <section>
-      <h2 class="title is-2">
-        Apps mobile
-      </h2>
-      <h5 class="subtitle is-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem suscipit, quo laborum fugiat repudiandae veritatis non! Eos aliquid quo beatae dicta facere voluptatibus suscipit quisquam natus mollitia possimus, culpa ratione?
-      </h5>
-    </section>
-  </div>
-
+  <Hero />
 </template>
 
 <script lang="ts">
@@ -40,10 +10,4 @@ export default Vue.extend({
 })
 </script>
 <style lang="scss" scoped>
-  .first-title {
-    margin-top: 4rem;
-  }
-  .second-title {
-    margin-top: 6rem;
-  }
 </style>
