33586487aa5b64202eddd01075af28451592520c
francisco <fr.betancourt@live.com>
2022-01-24 13:49:19 -0300

    Applied styles for centering CTA one elements.

--- components/CtaOne.vue
+++ components/CtaOne.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="container is-full-hd">
+    <section class="container is-full-hd is-flex">
         <h2 class="title is-2">
             CTA 1
         </h2>
-        <h5 class="subtitle is-5">
+        <h5 class="subtitle is-5 has-text-centered-desktop">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi aliquam asperiores quis illo accusantium ipsam temporibus magni, libero dignissimos sint error nostrum soluta voluptas eos ea illum, consectetur placeat ullam!
         </h5>
-        <b-button type="is-primary" rounded expanded>
+        <b-button type="is-primary" rounded>
             Haz contacto
         </b-button>
     </section>
@@ -19,9 +19,19 @@
 </script>
 
 <style lang="scss" scoped>
+    @import '~bulma/sass/utilities/mixins';
+
     section {
+        flex-direction: column;
         margin-top: 6rem;
         padding-left: 1rem;
         padding-right: 1rem;
     }
+
+    @include desktop {
+        section {
+            align-items: center;
+        }
+    }
+
 </style>
