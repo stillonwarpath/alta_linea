0c6505220cfb02123208d76ec47210424dee0fc9
francisco <fr.betancourt@live.com>
2022-01-25 13:09:19 -0300

    Added team member structure.

--- components/Team.vue
+++ components/Team.vue
@@ -3,7 +3,17 @@
         <h2 class="title is-2">
             Nosotros
         </h2>
-
+        <div class="columns">
+            <div class="column">
+                <b-image
+                    src="https://picsum.photos/id/358/314"
+                    alt="Team member image">
+                </b-image>
+                <h5 class="title is-5">
+                    Integrante 1
+                </h5>
+            </div>
+        </div> 
     </section>
 </template>
 
