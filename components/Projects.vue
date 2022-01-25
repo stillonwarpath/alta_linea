57dd46852f322b32433310cbc74b5e017a4b92a4
francisco <fr.betancourt@live.com>
2022-01-25 11:52:43 -0300

    Added project structure.

--- components/Projects.vue
+++ components/Projects.vue
@@ -3,6 +3,20 @@
         <h2 class="title is-2">
             Proyectos
         </h2>
+        <div class="columns is-multiline">
+            <div class="column is-half-desktop">
+                Proyecto 1
+            </div>
+            <div class="column is-half-desktop">
+                Proyecto 2
+            </div>
+            <div class="column is-half-desktop">
+                Proyecto 3
+            </div>
+            <div class="column is-half-desktop">
+                Proyecto 4
+            </div>
+        </div>
     </section>
 </template>
 
